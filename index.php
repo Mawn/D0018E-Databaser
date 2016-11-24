8bf402a7cb7c77475225cb2ac30f1bccfe52e6ea
Jesper Öhman <jesper.ohman.1@gmail.com>
2016-11-24 04:30:53 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -92,8 +92,6 @@
       <ol class="carousel-indicators">
         <li data-target="#carousel" data-slide-to="0" class="active"></li>
         <li data-target="#carousel" data-slide-to="1"></li>
-        <li data-target="#carousel" data-slide-to="2"></li>
-		<li data-target="#carousel" data-slide-to="3"></li>
       </ol>
       <div class="carousel-inner" role="listbox">
         <div class="item active">
@@ -101,28 +99,8 @@
             <img class="first-slide" src="BlackFriday.png" alt="First slide" width="100%"></img>
 		  </a>
         </div>
-        <div class="item">
-          <img class="second-slide" src="http://lorempixel.com/500/200/animals" alt="Second slide">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Headline</h1>
-              <p>Text</p>
-              <p><a class="btn btn-lg btn-primary" href="#" role="button">Placeholder</a></p>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <img class="third-slide" src="http://lorempixel.com/500/200/nature" alt="Third slide">
-          <div class="container">
-            <div class="carousel-caption">
-              <h1>Headline</h1>
-              <p>Text</p>
-              <p><a class="btn btn-lg btn-primary" href="#" role="button">Placeholder</a></p>
-            </div>
-          </div>
-        </div>
 		<div class="item">
-          <img class="fourth-slide" src="http://placecage.com/gif/500/200" alt="Fourth slide">
+          <img class="second-slide" src="http://placecage.com/gif/500/200" alt="Second slide"></img>
         </div>
       </div>
       <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
