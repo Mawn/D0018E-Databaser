ba3c73efc39b52843b431edc509718a99f8b05ea
Jesper Öhman <jesper.ohman.1@gmail.com>
2017-01-03 15:50:20 +0100

    Delete ordersdeleteall.php

--- ajax/ordersdeleteall.php
+++
@@ -1,8 +0,0 @@
-<?php
-  include("../connect.php");
-  if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'){
-    $userid = $_POST['userid'];
-    $sql = "DELETE FROM orders WHERE userid = '$userid'";
-    mysqli_query($conn, $sql);
-  }
-?>
